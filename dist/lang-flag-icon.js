34540ffa800eea011eca7aa0cf17fb5536d754e2
Niels Holt <nho@fcoo.dk>
2016-05-24 14:57:51 +0200

    $incl-table-display added to scss-file

--- dist/lang-flag-icon.js
+++ dist/lang-flag-icon.js
@@ -14,7 +14,7 @@
 	var ns = window;
 
 	function LangFlag( options ) {
-		this.VERSION = "0.1.4";
+		this.VERSION = "0.1.5";
 		this.options = $.extend({
 			//Default options
 			defaultFlag: 'dk',
