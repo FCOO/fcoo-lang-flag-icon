064fafffd5f85d9146de639c419cdea4a3d4ac26
Niels Holt <nho@fcoo.dk>
2016-05-24 15:44:58 +0200

    Renaming sass-variable

--- dist/lang-flag-icon.js
+++ dist/lang-flag-icon.js
@@ -14,7 +14,7 @@
 	var ns = window;
 
 	function LangFlag( options ) {
-		this.VERSION = "0.1.5";
+		this.VERSION = "0.1.6";
 		this.options = $.extend({
 			//Default options
 			defaultFlag: 'dk',
